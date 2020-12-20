4d09b9679973606e46a25cd092b59107f68a4edb
Anthony Fajri <anthony.fajri@gmail.com>
2020-12-20 08:42:34 -0800

    new bulkstat config method

--- gen_bulkstat_config.py
+++ gen_bulkstat_config.py
@@ -3,7 +3,7 @@
 import sys
 
 file = "bulkstat_scheme_all.log"
-number = 20
+
 schema = sys.argv[1]
 
 '''
@@ -18,27 +18,51 @@ bulkstat_file.close()
 
 num = 0
 line_num = 0
+max_len = 950
+
+start = True
+
 for line in bulkstat:
     line = ' '.join(line.split())
     sch = line.split(" ")[1]
     metric = line.split(" ")[2]
     
-    if sch == schema and line.split(" ")[5] != "Info":
-        if line_num % number == 0:
+    if sch == schema and line.split(" ")[5] != "Info":        
+        line_num = line_num+len(metric)
+        if start == True or line_num > max_len:
+            start = False
             num += 1
+            line_num = 0
             if schema == "apn":
-                print("{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%apn%,{}".format(schema, schema, num, schema, num, metric), end = ",")
+                print("\n{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%apn%,{}".format(schema, schema, num, schema, num, metric), end = "")
             elif schema == "radius":
-                print("{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%ipaddr%,{}".format(schema, schema, num, schema, num, metric), end = ",")
+                print("\n{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%ipaddr%,{}".format(schema, schema, num, schema, num, metric), end = "")
             elif schema == "radius-group":
-                print("{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%group%,{}".format(schema, schema, num, schema, num, metric), end = ",")                
+                print("\n{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%group%,{}".format(schema, schema, num, schema, num, metric), end = "")                
             elif schema == "saegw":
-                print("{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%saegw-servname%,{}".format(schema, schema, num, schema, num, metric), end = ",") 
+                print("\n{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%saegw-servname%,{}".format(schema, schema, num, schema, num, metric), end = "") 
+            elif schema == "card":
+                print("\n{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%card%,{}".format(schema, schema, num, schema, num, metric), end = "")                 
+            elif schema == "port":
+                print("\n{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%card%-%port%,{}".format(schema, schema, num, schema, num, metric), end = "")                 
+
             else:            
-                print("{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%servname%,{}".format(schema, schema, num, schema, num, metric), end = ",")
-        elif (line_num+1)%20 == 0:
-            print("{}".format(metric))
+                print("\n{} schema {}Sch{} format {}Sch{},%localdate%,%localtime%,%servname%\,{}".format(schema, schema, num, schema, num, metric), end = "")
         else:
-            print("{}".format(metric), end=",")
-        line_num +=1
+            print(",{}".format(metric), end="")
+    elif schema == "disconnectReason" and "disc-reason" in line.split(" ")[2] and line.split(" ")[2] != "%disc-reason-summary%":
+        '''
+        input: 1849  system    %disc-reason-681%                                             Int64     0   Counter
+        output:  schema disconnectReason9 format disconnectReason9,%localdate%,%localtime%,%disc-reason-400%
+        '''
+        line_num = line_num+len(metric)
+        if start == True or line_num > max_len:
+            start = False
+            num += 1
+            line_num = 0
+            print("\nschema disconnectReason{} format disconnectReason{},%localdate%,%localtime%,%apn%{}".format(num, num, metric), end = "")
+        else:
+            print(",{}".format(metric), end="")        
+
+
 
--- process_bulkstat.py
+++ process_bulkstat.py
@@ -30,6 +30,16 @@ bulkstat_config = {}
 output = {}
 
 def load_sch_to_mtric():
+    """
+    this function is to generate mapping between disconnect reason number and disconnect reason readable value
+    this is based on 
+    the mapping is:
+        disc-reason-0,Unknown
+        disc-reason-1,Admin-disconnect
+        disc-reason-2,Remote-disconnect
+        disc-reason-3,Local-disconnect
+        disc-reason-4,No-resource
+    """
     for schema in sch_to_metric:
         schema_name = schema.split(",")[0].strip()
         metric_name = schema.split(",")[1].strip()
@@ -37,9 +47,12 @@ def load_sch_to_mtric():
 
 def load_bulkstat_schema():
     """
+    This function is to load bulkstat schema data.
+    bulkstat schema data is an output of "show bulkstat schema"
+
     input:
     system       disconnectReason3        No          disconnectReason3,MME,%localdate%,%localtime%,%disc-reason-100%,%disc-reason-101%,%disc-reason-102%,%disc-reason-103%,%disc-reason-104%,%disc-reason-105%,%disc-reason-106%,%disc-reason-107%,%disc-reason-108%,%disc-reason-109%,%disc-reason-110%,%disc-reason-111%,%disc-reason-112%,%disc-reason-113%,%disc-reason-114%,%disc-reason-115%,%disc-reason-116%,%disc-reason-117%,%disc-reason-118%,%disc-reason-119%,%disc-reason-120%,%disc-reason-121%,%disc-reason-122%,%disc-reason-123%,%disc-reason-124%,%disc-reason-125%,%disc-reason-126%,%disc-reason-127%,%disc-reason-128%,%disc-reason-129%,%disc-reason-130%,%disc-reason-131%,%disc-reason-132%,%disc-reason-133%,%disc-reason-134%,%disc-reason-135%,%disc-reason-136%,%disc-reason-137%,%disc-reason-138%,%disc-reason-139%,%disc-reason-140%,%disc-reason-141%,%disc-reason-142%,%disc-reason-143%,%disc-reason-144%,%disc-reason-145%,%disc-reason-146%,%disc-reason-147%,%disc-reason-148%,%disc-reason-149%
-    output:
+    output as a dictionoary
     'disconnectReason8': {4: '%disc-reason-350%', 5: '%disc-reason-351%', 6: '%disc-reason-352%', 7: '%disc-reason-353%', 8: '%disc-reason-354%', 9: '%disc-reason-355%',
     """
     for line in bulkstat_schema:
@@ -53,72 +66,63 @@ def load_bulkstat_schema():
                 bulkstat_config[bulkstat_config_line[0]].setdefault(number, cfg)
 
 def load_bulkstat_data():
-        """
-                disconnectReason8,20201217,183214,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,91,0,0,12,0,0,110,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1
-                mmeSch56,20201217,221500,s1-mme,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-                radius-groupSch4,20201218,174640,%servname%,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
-        """
-        for line in bulkstat_data:
-            line = line.strip()
-            bulkstat_data_line = line.split(",")
-            schema_match = re.search(r"(^.*)Sch",line)
-            temp_output = {}
-            if schema_match:
-                schema = schema_match.group(1)
-            for number, data in enumerate(bulkstat_data_line):                
-                if schema not in output:
-                   output.setdefault(schema, {})
-                if number == 0:
-                    key = data
+    """
+    this function is to:
+        (1) load bulkstat data
+        (2) correlate bulkstat data with config
+        (3) generate PushGateway Temporary config file
+    disconnectReason8,20201217,183214,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2,0,91,0,0,12,0,0,110,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1
+    mmeSch56,20201217,221500,s1-mme,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    radius-groupSch4,20201218,174640,%servname%,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0
+    """
+    for line in bulkstat_data:
+        line = line.strip()
+        bulkstat_data_line = line.split(",")
+        schema_match = re.search(r"(^.*)Sch",line)
+        temp_output = {}
+        if schema_match:
+            schema = schema_match.group(1)
+        for number, data in enumerate(bulkstat_data_line):                
+            if schema not in output:
+                output.setdefault(schema, {})
+            if number == 0:
+                key = data
                 
 
-                #handle disconnect_reason
-                if "disconnect" in key:
-                    if key in bulkstat_config and number > 2 and data != "0":                                              
+            #handle disconnect_reason
+            if "disconnect" in key:
+                if key in bulkstat_config and number > 2 and data != "0":                                              
+                    config = bulkstat_config[key][number].replace("%","")
+                    metric = schema_to_metric[config]
+                    output_file.write("disconnectReason {{reason=\"{}\"}} {}\n".format(metric.replace("-","_"), data)) 
+            elif key == "ippoolSch1":
+                '''
+                Special condition to handle IP Pool
+                ippoolSch1,20201218,182500,pool-3669,0,0,0,244,10.66.0.11
+                '''                    
+                if number == 3:
+                    groupname = bulkstat_data_line[3]
+                elif number > 3:
+                    if data != '0' and data != "":
                         config = bulkstat_config[key][number].replace("%","")
-                        metric = schema_to_metric[config]
-                        output_file.write("disconnectReason {{reason=\"{}\"}} {}\n".format(metric.replace("-","_"), data)) 
-                elif key == "ippoolSch1":
-                    '''
-                    Special condition to handle IP Pool
-                    ippoolSch1,20201218,182500,pool-3669,0,0,0,244,10.66.0.11
-                    '''                    
-                    if number == 3:
-                        groupname = bulkstat_data_line[3]
-                    elif number > 3:
-                        if data != '0' and data != "" and data.isnumeric():
-                            config = bulkstat_config[key][number].replace("%","")
-                            output_file.write("ippool {{poolname=\"{}\", metric=\"{}\"}} {}\n".format(groupname.replace("-","_"), config.replace("-","_"), data)) 
-
-                elif key == "ippoolSch2":
-                    '''
-                    Special condition to handle IP Pool group
-                    ippoolSch2,20201218,182500,0,0,0,0,0,0,0,0
-                    '''                
-                    if(bulkstat_data_line[3] != "0"):
-                        if number == 3:
-                            groupname = bulkstat_data_line[3]
-                        elif number > 3:
-                            if data != '0' and data != "" and data.isnumeric():
-                                config = bulkstat_config[key][number].replace("%","")
-                                output_file.write("ippool_group {poolname = \"{}\", metric = \"{}\"} {}\n".format(groupname.replace("-","_"), config.replace("-","_"), data)) 
-                                            
-                else:
-                    if number ==3:
-                        identifier = data
-                    elif number > 3:
-                        if data != '0' and data != "" and data.isnumeric():
-                            config = bulkstat_config[key][number].replace("%","").split("-")
-                            string_output = "{} {{id=\"{}\"".format(schema.replace("-","_"), identifier.replace("-","_"))
-                            for num,met in enumerate(config):
-                                string_output = string_output + ", metric{}=\"{}\"".format(num, met)
-                            string_output = string_output + "}} {}\n".format(data)
-                            output_file.write(string_output)
+                        output_file.write("ippool {{poolname=\"{}\", metric=\"{}\"}} {}\n".format(groupname.replace("-","_"), config.replace("-","_"), data))                                        
+            else:
+                if number ==3:
+                    identifier = data
+                elif number > 3:
+                    if data != '0' and data != "" and data.isnumeric():
+                        config = bulkstat_config[key][number].replace("%","").split("-")
+                        string_output = "{} {{id=\"{}\"".format(schema.replace("-","_"), identifier.replace("-","_"))
+                        for num,met in enumerate(config):
+                            string_output = string_output + ", metric{}=\"{}\"".format(num, met)
+                        string_output = string_output + "}} {}\n".format(data)
+                        output_file.write(string_output)
                             
 
 
 def gen_pushgw_format():
     '''
+    This function is to load pushgateway config file into pushgateway
     cmd = 'cat tempfile.txt |  curl --data-binary @- http://' + pushgateway +'/metrics/job/bulkstat/node/' +node
     os.system(cmd)
     '''
